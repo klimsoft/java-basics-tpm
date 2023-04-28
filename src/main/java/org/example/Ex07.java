35bd4cee57103323e0ce53860cec18f004d5d071
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2023-04-28 10:27:51 +0200

    Ex 7

---
+++ src/main/java/org/example/Ex07.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String firstName = scanner.nextLine();
+        System.out.println("What is your last name?");
+        String lastName = scanner.nextLine();
+        System.out.println("What is your birth date?");
+        String birthDate = scanner.nextLine();
+
+        System.out.printf("New employee was created. First name: %s, last name: %s, born: %s", firstName, lastName, birthDate);
+
+    }
+}
