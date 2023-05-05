b33e25d125f6d6b320839f39cd3c3308f7aa5943
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2023-05-05 13:01:23 +0200

    Running tests

--- src/test/java/org/example/Ex19MockitoTest.java
+++ src/test/java/org/example/Ex19MockitoTest.java
@@ -7,7 +7,7 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Ex19Mockito {
+public class Ex19MockitoTest {
     @Test
     void testMockitoReturnList() {
         List mockedList = Mockito.mock();
